0c07989be507324249a2c5d3747b32bc0cfa6ef1
LaserPete <pete.t.doyle@gmail.com>
2022-01-04 14:39:02 +0100

    Fixed bluetooth formatting

--- src/Bluetooth.cpp
+++ src/Bluetooth.cpp
@@ -1,34 +1,37 @@
-#ifndef _BLUETOOTH_H_ 
+#ifndef _BLUETOOTH_H_
 #define _BLUETOOTH_H
 
-#include "header.h"
 #include <BluetoothSerial.h>
+
+#include "header.h"
 
 BluetoothSerial SerialBT;
 
-void setupBluetooth(){
+void setupBluetooth() {
   SerialBT.begin("ESP32Test");
-Serial.println("Bluetooth started, try to connect.");
+  Serial.println("Bluetooth started, try to connect.");
 }
 
-void writeToBluetoothSerial(SensorValues receivedValues){
-
-    SerialBT.print("Probe Temperature = ");
-    SerialBT.print(receivedValues.SHT31probeTemp);
-    SerialBT.println(" Celcius");
-    SerialBT.print("Probe Humidity = ");
-    SerialBT.print(receivedValues.SHT31probeHumidity);
-    SerialBT.println(" RH");
-    SerialBT.print("CO2 Level = ");
-    SerialBT.print(receivedValues.cO2Level);
-    SerialBT.println(" PPM");
-    SerialBT.print("Luminosity =");
-    SerialBT.print(receivedValues.luminosity);
-    SerialBT.println(" Lux");
-    SerialBT.println();
+void writeToBluetoothSerial(SensorValues receivedValues) {
+  SerialBT.print("Probe Temperature = ");
+  SerialBT.print(receivedValues.SHT31probeTemp);
+  SerialBT.println(" Celcius");
+  SerialBT.print("Probe Humidity = ");
+  SerialBT.print(receivedValues.SHT31probeHumidity);
+  SerialBT.println(" RH");
+  SerialBT.print("CO2 Level = ");
+  SerialBT.print(receivedValues.cO2Level);
+  SerialBT.println(" PPM");
+  SerialBT.print("Luminosity = ");
+  SerialBT.print(receivedValues.luminosity);
+  SerialBT.println(" Lux");
+  SerialBT.println();
 }
 
-void BTtoUART(){if(SerialBT.available()){
-  Serial.write(SerialBT.read());}}
+void BTtoUART() {
+  if (SerialBT.available()) {
+    Serial.write(SerialBT.read());
+  }
+}
 
 #endif
